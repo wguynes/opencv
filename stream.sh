a031f9b6ee48dc10e6aab82a040024b11448ea2e
William Guynes <wguynes@pillartechnology.com>
2017-03-13 12:56:02 -0500

    Start VLC and raspivid stream simultaneously

--- stream.sh
+++ stream.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+VLC_APPNAME='VLC'
+VIDEO_CAMERA_FQDN='wguynes-rpi.local'
 PORT=8090
 
-#ssh rpi -T <<EOT
-#    /usr/bin/raspivid \
-#        -o - \
-#        -t 0 \
-#        -w 640 -h 360 \
-#        -fps 25 \
-#        | \
-#        cvlc -vvv stream:///dev/stdin \
-#        --sout '#standard{access=http,mux=ts,dst=:${PORT}}' :demux=h264
-#EOT 
+function kill_vlc {
+    trap '' SIGINT
+    kill -KILL $(pgrep ${VLC_APPNAME})
+}
+trap kill_vlc SIGINT
 
-ssh rpi -T <<EOSSH
-COUNTER=0
-while true
-do
-    echo \${COUNTER}
-    sleep 1
-    let COUNTER=COUNTER+1 
-done
-EOSSH
+(
+    open /Applications/${VLC_APPNAME}.app --args http://${VIDEO_CAMERA_FQDN}:${PORT}/
+) &
+
+ssh -t -t rpi "
+    /usr/bin/raspivid \
+        -o - \
+        -t 0 \
+        -w 640 -h 360 \
+        -fps 25 \
+        | \
+        cvlc -vvv stream:///dev/stdin \
+        --sout '#standard{access=http,mux=ts,dst=:${PORT}}' :demux=h264
+"
+
+kill_vlc
