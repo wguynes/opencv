07e5d0e3f9dd6fd499531f1341874ba7a3065430
William Guynes <wguynes@pillartechnology.com>
2017-03-15 11:47:43 -0500

    More variables

--- stream.sh
+++ stream.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
 VLC_APPNAME='VLC'
-VIDEO_CAMERA_FQDN='wguynes-rpi.local'
-PORT=8090
+RPI_USERNAME='pi'
+RPI_FQDN='wguynes-rpi.local'
+RPI_PORT=8090
+FRAMES_PER_SECOND=25
 
 function kill_vlc {
     trap '' SIGINT
-    kill -KILL $(pgrep ${VLC_APPNAME})
+    VLC_PID=$(pgrep ${VLC_APPNAME})
+    if [ -n ${VLC_PID} ]
+    then
+        kill -KILL ${VLC_PID}
+    fi
 }
 trap kill_vlc SIGINT
 
 (
     open /Applications/${VLC_APPNAME}.app --args \
-        http://${VIDEO_CAMERA_FQDN}:${PORT}/
+        http://${RPI_FQDN}:${RPI_PORT}/
 ) &
 
-ssh -t -t rpi " \
-    /usr/bin/raspivid \
+ssh -t -t "${RPI_USERNAME}@${RPI_FQDN}" " \
+    raspivid \
         --output - \
-        --nopreview \
+        --width 800 --height 480 \
+        --rotation 270 \
         --timeout 0 \
-        --framerate 25 \
+        --framerate ${FRAMES_PER_SECOND} \
+        --metering backlit \
         | \
-        cvlc -vvv stream:///dev/stdin \
-        --sout '#standard{access=http,mux=ts,dst=:${PORT}}' \
+        cvlc \
+        -vvv \
+        stream:///dev/stdin \
+        --sout '#standard{access=http,mux=ts,dst=:${RPI_PORT}}' \
         :demux=h264 \
 "
 
