ff1a9471041f3bec18a45dad834f1caf9205c2af
William Guynes <wguynes@pillartechnology.com>
2017-03-24 13:01:18 -0500

    Refactor package.sh

--- package.sh
+++ package.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-usage()
-{
+source utilities.sh
+
+if [[ "$(os_type)" != 'raspbian' ]]
+then
     echo 'Package only for Raspbian'
     exit 1
-}
-
-if [[ "$(uname)" != "Linux" ]] || [[ ! -x /usr/bin/lsb_release ]]
-then
-    usage
 fi
 
-/usr/bin/lsb_release -a 2>/dev/null | grep 'Raspbian' >/dev/null 2>&1
-ec=$?
-if [[ $ec -ne 0 ]]
-then
-    usage
-fi
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-pushd ${SCRIPT_DIR}/src >/dev/null 2>&1
+pushd "$(script_dir)/src" >/dev/null 2>&1
 debuild -b -us -uc
 popd >/dev/null 2>&1
